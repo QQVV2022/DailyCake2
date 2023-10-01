a96bc6700918ca6db810909f4b7f6246ddff7b2f
Quinnll <qliu9467@gmail.com>
2023-10-01 12:05:19 -0700

    imgs

--- shop/tests.py
+++ shop/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+# Create a django unit test class inheriting from the TestCase class
+#
+
+class Unit_Test(TestCase):
+
+    # create a test for User
+    def test_unit_test(self):
+        # Assert that the test is working
+        self.assertEqual(1, 1)
+
